e48b623503adb7b9100543793b74bc8017abb356
hussainabid7199 <hussainabid7199@gmail.com>
2022-06-07 21:33:09 +0530

    new update

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,18 @@
-document.getElementsByClassName("content-inner").addEventListener("load").style.
+// var button = document.getElementById("tile-btn").length;
+
+// for(i=0; i<=button; i++){
+//     document.querySelectorAll("#tile-btn")[i].addEventListener("click").style.color = "red";
+// }
+ 
+ 
 
 
+
+// document.querySelectorAll(".tile-inner-element").addEventListener("click").style.backgroundColor = "red";
+
+var element = document.getElementById("tile-btn");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("tile-btn").style.color = "red";
+}
