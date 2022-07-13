d6fedcde34b161a67eefa682ff7959a49a5837cb
hussainabid7199 <hussainabid7199@gmail.com>
2022-07-13 16:03:53 +0530

    js animation update

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,18 +1,45 @@
-// var button = document.getElementById("tile-btn").length;
+let contactHome = document.querySelector(".contactHome").addEventListener("click", function(){
+    window.open("contactus.html", "_self");
+});
 
-// for(i=0; i<=button; i++){
-//     document.querySelectorAll("#tile-btn")[i].addEventListener("click").style.color = "red";
-// }
- 
- 
+var tileBtn = document.querySelectorAll(".tile-inner-element");
 
-
-
-// document.querySelectorAll(".tile-inner-element").addEventListener("click").style.backgroundColor = "red";
-
-var element = document.getElementById("tile-btn");
-element.addEventListener("click", myFunction);
-
-function myFunction() {
-  document.getElementById("tile-btn").style.color = "red";
+for(i=0; i<tileBtn.length; i++){
+    tileBtn[i].addEventListener("click", function(){
+        window.open("service.html", "_self");
+    });
 }
+
+
+var tileBtn = document.querySelectorAll(".tile-btn");
+
+for(i=0; i<tileBtn.length; i++){
+    tileBtn[i].addEventListener("click", function(){
+        window.open("service.html", "_self");
+    });
+}
+
+
+var tileBtnArrow = document.querySelectorAll(".tile-inner-element");
+
+for(i=0; i<tileBtnArrow.length; i++){
+    tileBtnArrow[i].addEventListener("mouseout", function(){
+        this.style.transition = ".9s";
+    });
+    tileBtnArrow[i].addEventListener("mouseover", function(){
+        this.style.transition = ".5s";
+    });
+}
+
+let classAdd = document.querySelectorAll("#bi");
+for(i=0; i<classAdd.length; i++){
+    classAdd[i].addEventListener("mouseover", function(){
+        this.style.color = "white";
+        this.style.transition = ".5s";
+    })
+    classAdd[i].addEventListener("mouseout", function(){
+        this.style.color = "black";
+        this.style.transition = ".8s";
+    })
+}
+        
